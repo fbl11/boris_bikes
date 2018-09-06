e8c915dfa3368fe9e8a4278aa437868ac4688908
fbl11 <freyab_london@yahoo.co.uk>
2018-09-06 13:52:37 +0100

    Complete step 14 - store a maximum of 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,19 @@
  require_relative './bike'
 
 class DockingStation
-	attr_reader :bike
+	attr_reader :bikes
+
+	def initialize
+		@bikes = []
+	end
 
 	def dock_bike(bike)
-		fail 'Docking station full' unless (@bike == nil)
-		@bike = bike
+		fail 'Docking station full' if @bikes.length == 20
+		@bikes.push(bike)
 	end
+
 	def release_bike
-		raise 'No bikes available' unless @bike
-		@bike
-	end
-	def bike
-       @bike
+		raise 'No bikes available' if @bikes.empty?
+		@bikes.pop
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,41 +2,46 @@ require 'docking_station'
 
 describe DockingStation do
 
-	# it 'responds to release_bike' do
-	# 	expect(subject).to respond_to(:release_bike)
-	# end
 	it { is_expected.to respond_to(:release_bike) }
 	it 'releases working bike' do
 		bike = Bike.new
 		subject.dock_bike(bike)
 		expect(subject.release_bike).to be_working
 	end
+
 	describe '#release_bike' do
 		it 'releases existing bike' do
 			bike = Bike.new
 			subject.dock_bike(bike)
-			expect(subject.release_bike).to eq(bike)
+			rel_bike = subject.release_bike
+			expect(rel_bike).to eq(bike)
 		end
 		it 'raises an error when there are no bikes available' do
-		  expect { subject.release_bike } .to raise_error("No bikes available")
+		  expect { subject.release_bike }.to raise_error("No bikes available")
 	    end
+# example implementation of expect when called with a block
+   # def expect(&block)
+   # 	 begin
+   # 	 	block.yield
+   # 	 rescue Exception => ex
+   #      ToMatch.new(ex)
+   # 	 end
+   # end
 
 	end
 
 	describe '#dock_bike' do
-
 		it { is_expected.to respond_to(:dock_bike) }
 		it { is_expected.to respond_to(:dock_bike).with(1).argument }
-    	it { is_expected.to respond_to(:bike) }
+    	it { is_expected.to respond_to(:bikes) }
     	it 'adds bike to docking station' do
 		  bike = Bike.new
 		  subject.dock_bike(bike)
-		  expect(subject.bike).to eq bike
+		  expect(subject.bikes).to eq([bike])
     end
         it 'raises an error when full and user tries to dock_bike' do
-        	bike = Bike.new
-        	subject.dock_bike(bike)
-        	expect { subject.dock_bike(bike) } .to raise_error("Docking station full")
+        	20.times {subject.dock_bike(Bike.new)}
+        	expect {subject.dock_bike(Bike.new)} .to raise_error("Docking station full")
         end
 	end
 
