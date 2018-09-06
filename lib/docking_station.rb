6158a3829f1ef58e8c965076cd0141d0ca6d65d1
fbl11 <freyab_london@yahoo.co.uk>
2018-09-06 16:22:56 +0100

    Solution Step 17 - initialization defaults

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,11 @@
  require_relative './bike'
 
 class DockingStation
-	attr_reader :bikes
+	attr_reader :bikes, :capacity
 	DEFAULT_CAPACITY = 20
-	def initialize
+
+	def initialize(capacity = DEFAULT_CAPACITY)
+		@capacity = capacity
 		@bikes = []
 	end
 
@@ -19,7 +21,7 @@ class DockingStation
 
 	private
 	def full?
-		@bikes.length == DEFAULT_CAPACITY
+		@bikes.length >= @capacity
 	end
 
 	def empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,13 @@ require 'docking_station'
 
 describe DockingStation do
 
+	it { is_expected.to respond_to(:dock_bike) }
+	it { is_expected.to respond_to(:dock_bike).with(1).argument }
 	it { is_expected.to respond_to(:release_bike) }
+	it "initialises a docking station with default capacity" do
+		expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+ 	end
+
 	it 'releases working bike' do
 		bike = Bike.new
 		subject.dock_bike(bike)
@@ -13,12 +19,36 @@ describe DockingStation do
 		it 'releases existing bike' do
 			bike = Bike.new
 			subject.dock_bike(bike)
-			rel_bike = subject.release_bike
-			expect(rel_bike).to eq(bike)
+			expect(subject.release_bike).to eq(bike)
 		end
 		it 'raises an error when there are no bikes available' do
 		  expect { subject.release_bike }.to raise_error("No bikes available")
 	    end
+
+	end
+
+	describe '#dock_bike' do
+    	it { is_expected.to respond_to(:bikes) }
+    	it 'adds bike to docking station' do
+		  bike = Bike.new
+		  subject.dock_bike(bike)
+		  expect(subject.bikes).to eq([bike])
+    end
+        it 'raises an error when full and user tries to dock_bike' do
+        	(subject.capacity).times {subject.dock_bike(Bike.new)}
+        	expect {subject.dock_bike(Bike.new)} .to raise_error("Docking station full")
+        end
+	end
+
+end
+
+	# it 'releases existing bike' do
+	# 		bike = Bike.new
+	# 		subject.dock_bike(bike)
+	# 		rel_bike = subject.release_bike
+	# 		expect(rel_bike).to eq(bike)
+	# 	end
+
 # example implementation of expect when called with a block
    # def expect(&block)
    # 	 begin
@@ -27,22 +57,3 @@ describe DockingStation do
    #      ToMatch.new(ex)
    # 	 end
    # end
-
-	end
-
-	describe '#dock_bike' do
-		it { is_expected.to respond_to(:dock_bike) }
-		it { is_expected.to respond_to(:dock_bike).with(1).argument }
-    	it { is_expected.to respond_to(:bikes) }
-    	it 'adds bike to docking station' do
-		  bike = Bike.new
-		  subject.dock_bike(bike)
-		  expect(subject.bikes).to eq([bike])
-    end
-        it 'raises an error when full and user tries to dock_bike' do
-        	DockingStation::DEFAULT_CAPACITY.times {subject.dock_bike(Bike.new)}
-        	expect {subject.dock_bike(Bike.new)} .to raise_error("Docking station full")
-        end
-	end
-
-end
